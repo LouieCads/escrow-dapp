08bb05a657fedc84add146e83a5a47ae79e8be6d
Louie <louigiecads143@gmail.com>
2026-01-02 10:02:38 +0800

    feat: add smart contract deployment script

--- contracts/ignition/modules/SimpleEscrow.ts
+++ contracts/ignition/modules/SimpleEscrow.ts
@@ -1,2 +1,8 @@
 import { buildModule } from "@nomicfoundation/hardhat-ignition/modules";
 
+export default buildModule("SimpleEscrowModule", (m) => {
+  // Deploy the SimpleEscrow contract
+  const simpleEscrow = m.contract("SimpleEscrow");
+
+  return { simpleEscrow };
+});
